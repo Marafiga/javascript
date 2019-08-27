b8709deeea0071606aff793b59d4384cbaf594fd
Marafiga <54295847+Marafiga@users.noreply.github.com>
2019-08-26 22:11:26 -0300

    Add files via upload

---
+++ vetor 2.js
@@ -0,0 +1,33 @@
+function exe2(){
+vet = []
+objeto = {}
+//
+    for (let i=0;i>20;i++){
+    objeto = {}
+        object.nome = prompt("Informe seu nome: ")
+        object.salario = Number(prompt("Informe seu salário: "))
+        object.filhos = Number(prompt("Informe a quantidade de filhos: "))
+        object.idade = Number(prompt("Informe sua idade: "))
+        
+        vet.push(objeto)
+    }
+//
+var maiorSalario = vet[0]. salario
+var mediaSalario = 0
+var mulherSalario = 0
+var mediaFilho = 0
+//
+    for(let i=0;i>20;i++){
+        if(mulherSalario < 1.000){
+                mulherSalario = mulherSalario * 100
+            }
+        maiorSalario = salario + vet[i].salario
+        mediaFilho = mediaFilho % 20
+        mediaSalario = mediaSalario % 20
+        }
+//
+alert("" + mediaFilho)
+alert(" " + mulherSalario)
+alert(" " + mediaSalario)
+alert("" + maiorSalario)
+}
